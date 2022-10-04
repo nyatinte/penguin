7ec02dbe4405aa2cc14d236e5aadce97c169f7d6
nyatinte <takumi.062757@gmail.com>
2022-10-04 20:34:13 +0900

    フロントエンドにAuth0を導入

--- services/penguin-frontend/pages/_app.tsx
+++ services/penguin-frontend/pages/_app.tsx
@@ -1,12 +1,17 @@
-import type { AppProps } from "next/app";
-import { ChakraProvider } from "@chakra-ui/react";
+import type { AppProps } from 'next/app'
+import { ChakraProvider } from '@chakra-ui/react'
+import { UserProvider } from '@auth0/nextjs-auth0'
+import Header from '../components/Header'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
-      <Component {...pageProps} />
-    </ChakraProvider>
-  );
+    <UserProvider>
+      <ChakraProvider>
+        <Header />
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </UserProvider>
+  )
 }
 
-export default MyApp;
+export default MyApp
